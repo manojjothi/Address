4e8f37c0b68a16742737bd4e8e5d9c00a99f4b5d
Manojviji@93 <manojjothi1992@gmail.com>
2021-11-25 08:26:27 +0530

    First commit

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,29 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+		System.out.println("Greens OMR Address is : No.20 ABCD street");
+
+	}
+private void greensAdyar() {
+	System.out.println("Greens Adyar Address is : No.100 XYZ street");
+
+}
+    private void greensTambaram() {
+       System.out.println("Greens Tambaram Address is : No.30 XXX street");
+	}
+    private void greensVelachery() {
+       System.out.println("Greens Velachery Address is : No.50 YYYY street");
+	}
+    private void greensAnnanagar() {
+       System.out.println("Greens Annanagar Address is : No.140 ZZZZZ street");
+	}
+    public static void main(String[] args) {
+		GreensTech Add = new GreensTech();
+		Add.greensOmr();
+		Add.greensAdyar();
+		Add.greensTambaram();
+		Add.greensVelachery();
+		Add.greensAnnanagar();
+	}
+}
